98b5de8c5e68827f8a303beb2fe99d3bb6ab69d0
Devansh <dev000041995@gmail.com>
2020-01-04 18:24:17 +0530

    details

--- app/src/main/java/project/com/yuvaaag/fragment/HomeFragment.java
+++ app/src/main/java/project/com/yuvaaag/fragment/HomeFragment.java
@@ -1,9 +1,11 @@
 package project.com.yuvaaag.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
@@ -12,9 +14,12 @@ import java.util.ArrayList;
 
 import project.com.yuvaaag.Adapter.ViewPagerAdapter;
 import project.com.yuvaaag.R;
+import project.com.yuvaaag.newsinvideo.NewsInVideo;
 
 
 public class HomeFragment extends Fragment {
+
+    private RelativeLayout newsOneRel;
 
     public static HomeFragment newInstance(int page, String title) {
         HomeFragment fragmentFirst = new HomeFragment();
@@ -32,8 +37,14 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(
                 R.layout.home_fragment, container, false);
-
-
+        newsOneRel=(RelativeLayout)view.findViewById(R.id.newsOneRel);
+        newsOneRel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(), NewsInVideo.class);
+                startActivity(intent);
+            }
+        });
         viewpagerStory=view.findViewById(R.id.viewpagerStory);
         viewpagerStory.setAdapter(new ViewPagerAdapter(getActivity()));
 
