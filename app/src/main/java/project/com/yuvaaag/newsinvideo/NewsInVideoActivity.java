82b428cd2e3999f1acab9ac76e0b1959524f794d
Devansh <dev000041995@gmail.com>
2020-01-04 18:26:37 +0530

    details

---
+++ app/src/main/java/project/com/yuvaaag/newsinvideo/NewsInVideoActivity.java
@@ -0,0 +1,16 @@
+package project.com.yuvaaag.newsinvideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import project.com.yuvaaag.R;
+
+public class NewsInVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_in_video);
+    }
+}
